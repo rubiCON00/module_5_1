90018d7c68b553af6052cabd705ebc9069405fe0
rubiCON00 <zevs9905@gmail.com>
2024-10-12 23:14:00 +0300

    Update module_5_1.py

--- module_5_1.py
+++ module_5_1.py
@@ -17,20 +17,33 @@ class House:
         return self.number_of_floors
     def __str__(self):
         return (f"Название:{self.name},кол-во этажей:{self.number_of_floors}")
+
     def __eq__(self, other):
-        return self.number_of_floors == other.number_of_floors
+        if isinstance(other, House):
+            return self.number_of_floors == other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors == other
     def __lt__(self, other):
-        return self.number_of_floors < other.number_of_floors
+        if isinstance(other, House):
+            return self.number_of_floors < other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors < other
     def __le__(self, other):
-        return self.number_of_floors <= other.number_of_floors
+        if isinstance(other, House):
+            return self.number_of_floors <= other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors <= other
     def __gt__(self, other):
-        return self.number_of_floors > other.number_of_floors
+        return self.__le__(other)
     def __ge__(self, other):
-        return self.number_of_floors >= other.number_of_floors
+        return not self.__lt__(other)
     def __ne__(self, other):
-        return self.number_of_floors != other.number_of_floors
+        return not self.__eq__(other)
     def __add__(self, value):
-        self.number_of_floors = self.number_of_floors + value
+        if isinstance(value, int):
+            self.number_of_floors += value
+        elif isinstance(value, House):
+            self.number_of_floors += value.number_of_floors
         return self
 
 
